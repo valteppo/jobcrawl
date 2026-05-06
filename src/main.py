755a77219f81e7f0efe3f9561985518daa68cbcf
Valtteri Teppo <vamteppo@protonmail.com>
2026-05-06 21:06:38 +0300

    Lean

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 import os
 from net import Netparser
-from data import load_saved_listing_information, JobListing
 
 AI_AGENT_LIGHT = os.environ.get('AI_AGENT_LIGHT')
 AI_AGENT_HEAVY= os.environ.get('AI_AGENT_HEAVY')
